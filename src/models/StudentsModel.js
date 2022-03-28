219b17129a276b80246c8de084e0926ca2568862
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-03-29 04:15:19 +0600

    user create just done

--- src/models/StudentsModel.js
+++ src/models/StudentsModel.js
@@ -4,8 +4,18 @@ const DataSchema = mongoose.Schema({
     Name:String,
     Roll:String,
     Class:String,
-    Remarks:String
-})
+    Remarks:{type:String, default:"no remarks"},
+    // custom Validation
+    Mobile:{
+        type:String,
+        validate:{
+            validator:function(value){
+                return /(^(\+88|0088)?(01){1}[3456789]{1}(\d){8})$/.test(value)
+            },
+            message: "11 Digit Mobile Number Required @ripon"
+        }
+    }
+}, {versionKey:false})
 
 const StudentsModel = mongoose.model('students', DataSchema);
 
